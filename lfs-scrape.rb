1e628b305e1afec60c8efc222d493bf14b998d2e
Tobias Gerschner <tobias at yoper-linux dot org>
2012-05-07 20:10:03 +1200

    * catch a few more corner cases

--- lfs-scrape.rb
+++ lfs-scrape.rb
@@ -6,26 +6,27 @@ require 'open-uri'
 require 'awesome_print'
 
 @lfs_branch = 'stable'
-@smp_mflags = '-j24'
 
-
-
+# set smp flags based on a conversation with Con Kolivas 
+@smp_mflags = "-j`awk '/^processor/ { N++} END { print N*3 }' /proc/cpuinfo`"
 
 @output_dir = '/tmp/lfs/' + @lfs_branch + '/' + Time.now.strftime("%F")
-
 @lfs_base_url = "http://www.linuxfromscratch.org/lfs/view/"
 @lfs_index_url = @lfs_base_url + @lfs_branch
 
 # likely to be further abstracted, but let's keep it KISS for now
 
-def extract_chapter5(chapter)
+def extract_chapter(article)
+  article_url = @lfs_base_url + "/#{@lfs_branch}/" + article
+  puts "fetching : #{article_url} .. "
+  article_content= Nokogiri::HTML(open(article_url))
+
   script = Array.new
-#   puts "one more entry to chapter 5:"
-  chapter_url = @lfs_base_url + "/#{@lfs_branch}/" + chapter
-  puts "fetching : #{chapter_url} .. "
-  chapter = Nokogiri::HTML(open(chapter_url))
-  chapter.css('pre.userinput').each {|my_match|script << my_match.content}
+  article_content.css('pre.userinput').each {|my_match|script << my_match.content}
+  
+  script = NIL unless script.size > 0
   script
+  
 end
 
 def write_out_script(node_title)
@@ -38,20 +39,20 @@ def write_out_script(node_title)
     f.write("#!/tools/bin/bash -xe\n")
     f.write("# #{script_name}\n")
 
-    f.write("cd /mnt/lfs/sources\n")
+    f.write("cd #{@script_prefix}/sources\n")
     f.write("tar_name=`ls #{pkg_name}*tar.*`\n")
     f.write("if [ -n \"$tar_name\" ]\n")
     f.write("then\n")
-    f.write("  cd /mnt/lfs/builds\n")
-    f.write("  tar -xf /mnt/lfs/sources/$tar_name\n")
-    f.write("  dir_name=`tar -tf /mnt/lfs/sources/$tar_name | head -n 1|awk -F'/' '{print $1}'`\n")
+    f.write("  cd #{@script_prefix}/builds\n")
+    f.write("  tar -xf #{@script_prefix}/sources/$tar_name\n")
+    f.write("  dir_name=`tar -tf #{@script_prefix}/sources/$tar_name | head -n 1|awk -F'/' '{print $1}'`\n")
     f.write("  cd $dir_name\n")
     f.write("fi\n")
     
     @script_code.each {|line|
                        line = "make #{@smp_mflags}" if line == "make"
-                       line = line.gsub(/patch -Np1 -i ../, 'patch -Np1 -i /mnt/lfs/sources')
-                       line = line.gsub(/tar -[a-zA-Z]* ../, 'tar -xf /mnt/lfs/sources')
+                       line = line.gsub(/patch -Np1 -i ../, "patch -Np1 -i #{@script_prefix}/sources")
+                       line = line.gsub(/tar -[a-zA-Z]* ../, "tar -xf #{@script_prefix}/sources")
                        line.strip!
                        f.write("#{line}\n")
                       }
@@ -67,11 +68,17 @@ doc = Nokogiri::HTML(open(@lfs_index_url))
 doc.xpath('//li/a').each do |node|
   
   if node.key?('href') && node.values.first.match('chapter0')
-#     puts "Shall we extract #{node.text} ? ..."
-    if node.values.first.match('chapter05')
-      @script_dir = @output_dir + '/chapter05'
+    if node.values.first.match(/chapter0[5,6]/)
+      @chapter = node.values.first.split('/').first
+      if @chapter == 'chapter05'
+	@script_prefix = '/mnt/lfs'
+      elsif @chapter == 'chapter06'
+	@script_prefix = ''
+      end
+      @script_dir = @output_dir + '/' + @chapter
       FileUtils.mkdir_p(@script_dir) unless Dir.exists?(@script_dir)
-      @script_code = extract_chapter5 node.values.first 
+      @script_code = extract_chapter node.values.first
+#       ap @script_code
       write_out_script node.text if @script_code
     end
   end
